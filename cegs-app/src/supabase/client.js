66f1ebcc6a56867fc822ad4ebf0c43e5e5569288
ejsczerniak <ejsczerniak@minha.fag.edu.br>
2026-07-09 20:08:36 -0300

    atualizando credenciais supabase para deploy

--- cegs-app/src/supabase/client.js
+++ cegs-app/src/supabase/client.js
@@ -1,17 +1,14 @@
 // ------------------------------------------------------------------
 // Arquivo: gofora/cegs-app/src/supabase/client.js
 // Descrição: Configuração do Cliente Supabase
-// Data: 28.09.2025 
-// Última modificação: 28.09.2025 
 // ------------------------------------------------------------------   
 
-// ➤ Importações de Instâncias de Cliente (createClient)
-// ✦ Supabase ⟶ Operações como Selecionar, Inserir, Atualizar, Deletar
 import { createClient } from '@supabase/supabase-js'
 
-// ➤ Instância de Exportação Supase
-// ✦ Exportando ⟶ URL e KEY
-export const supabase = createClient(
-    'https://kcogilktcfzfskydwpry.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtjb2dpbGt0Y2Z6ZnNreWR3cHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5OTMzNjMsImV4cCI6MjA2NjU2OTM2M30.nzicy5AkNm3wh80XvuxEcunu8RE-CLSEeVvQIaM6AmU'
-)
+// A URL do seu novo projeto
+const SUPABASE_URL = 'https://qmbpifhbbllurgcshupx.supabase.co'
+
+// A chave pública (anon key) que você forneceu
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtYnBpZmhiYmxsdXJnY3NodXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MTg5OTAsImV4cCI6MjA5OTE5NDk5MH0.vKYo5JX0rnOiH8s9ifm0qA6uMQdTSwzZGq6VZuxuaBQ'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
