0e955f8776a28055df12fff2a6ad69761ee82dd8
ejsczerniak <ejsczerniak@minha.fag.edu.br>
2026-07-09 21:04:52 -0300

    atualizando credenciais supabase para deploy

--- backend/app/etapas.py
+++ backend/app/etapas.py
@@ -13,7 +13,7 @@ from fastapi import APIRouter, HTTPException
 # ➤ Importações de Instâncias de Cliente
 # ✦ Supabase ⟶ Operações como Selecionar, Inserir, Atualizar, Deleta
 # ✦ Datetime ⟶ Módulo Padrão Python para Manipulação de Datas e Horários
-from backend.app.supabase_client import supabase
+from app.supabase_client import supabase
 from datetime import date
 
 # -------------------------------------------------------------------
