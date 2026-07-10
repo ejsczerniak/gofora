d73152a4aaf6e36957b874f70d95df92189dc4d2
ejsczerniak <ejsczerniak@minha.fag.edu.br>
2026-07-09 21:35:01 -0300

    atualizando credenciais supabase para deploy

--- cegs-app/src/supabase/client.js
+++ cegs-app/src/supabase/client.js
@@ -1,6 +1,14 @@
+// ------------------------------------------------------------------
+// Arquivo: gofora/cegs-app/src/supabase/client.js
+// Descrição: Configuração do Cliente Supabase
+// ------------------------------------------------------------------   
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
+// A URL do seu novo projeto
+const SUPABASE_URL = 'https://qmbpifhbbllurgcshupx.supabase.co' 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// A chave pública (anon key) que você forneceu
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtYnBpZmhiYmxsdXJnY3NodXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MTg5OTAsImV4cCI6MjA5OTE5NDk5MH0.vKYo5JX0rnOiH8s9ifm0qA6uMQdTSwzZGq6VZuxuaBQ'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
